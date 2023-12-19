b8e2540e0413aa2e043ad5e1e6802701e366d87a
jsmile <jsmile@naver.com>
2023-12-19 11:30:47 +0900

    riverpod AsyncNotifier Provider starter commit

---
+++ lib/src/app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'pages/counter/counter_page.dart';
+import 'widgets/custom_button.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AsyncNotifierProvider',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AsyncNotifierProvider'),
+      ),
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          shrinkWrap: true,
+          children: const [
+            CustomButton(
+              title: 'Counter',
+              child: CounterPage(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
